20daa0fb0f510ceb83674db490d7360bca16acb6
Ian <ianmcm.89@gmail.com>
2021-10-03 05:34:28 -0400

    commit tree 45e8b93e51ea882c36020a241efd32c62dd40cd4

--- src/index.js
+++ src/index.js
@@ -12,4 +12,92 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  getToys();
 });
+
+// Submit button event listener:
+document.querySelector("form.add-toy-form").addEventListener("submit", handleSumbit);
+
+// Submit button event handler function:
+function handleSumbit(e) {
+  e.preventDefault();
+  let toyObject = {
+    id:e.target.id,
+    name:e.target.name.value,
+    image:e.target.image.value,
+    likes: 0
+  }
+
+  createCard(toyObject);
+  postCard(toyObject);
+}
+
+// Fetch all toy objects:
+function getToys() {
+  fetch('http://localhost:3000/toys')
+    .then(res => res.json())
+    .then(json => json.forEach(toy => createCard(toy)))
+};
+
+//Create a new card div for each toy object in json and append each div to the toy collection::
+function createCard(toy) {
+  let div =document.createElement('div');
+  div.className = 'card';
+  div.innerHTML = `
+    <h2>${toy.name}</h2>
+    <img src="${toy.image}" class="toy-avatar" />
+    <p>${toy.likes} Likes</p>
+    <button class="like-btn" id="${toy.id}">Like</button>
+  `
+  // Add event listener to like button:
+  div.querySelector('button.like-btn').addEventListener('click', () => {
+    toy.likes++
+    div.querySelector('p').textContent = toy.likes + ' Likes';
+    updateLikes(toy);
+  });
+
+  // Append new div card to collection:
+  document.querySelector('div#toy-collection').appendChild(div);
+}
+
+// POST a new toy to the toy collection:
+function postCard(toy) {
+  fetch('http://localhost:3000/toys',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(toy)
+  })
+  .then(res => res.json())
+  .then(toy => console.log(toy));
+}
+
+// PATCH a toy's likes in the collection:
+function updateLikes(toy) {
+  fetch(`http://localhost:3000/toys/${toy.id}`,{
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(toy)
+  })
+  .then(res => res.json())
+  .then(toy => console.log(toy));
+}
+
+// DELETE a toy from the toy collection:
+function deleteCard(toy) {
+  fetch(`http://localhost:3000/toys/${toy.id}`,{
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify(toy)
+  })
+  .then(res => res.json())
+  .then(toy => console.log(toy));
+}
